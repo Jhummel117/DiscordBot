e7af348e743b2913adae530566cac6ac2b410216
Jhummel117 <67962979+Jhummel117@users.noreply.github.com>
2024-10-09 19:03:38 -0400

    Update blacklistShow.js

--- src/functions/blacklistShow.js
+++ src/functions/blacklistShow.js
@@ -1,21 +1,24 @@
-const User = require("discord.js");
-const blacklistSchema = require("../models/blacklistSchema");
+const blacklistSchema = require('../models/blacklistSchema');
+const { userMention } = require('discord.js');
 
 async function showBlacklistDB(client) {
     const doc = await blacklistSchema.blackListDB.findOne();
     const userList = [];
+
     if (doc) {
         const blacklistedUsers = doc.blackListedUsers;
 
-        // for every id in the database
-        for (const id of blacklistedUsers) {
-            const user = await client.users.fetch(id);
-            // if there is a user add them to the list with the tag
+        // Iterate through the blacklisted users and fetch their details
+        for (const { userId, role, duration } of blacklistedUsers) {
+            const user = await client.users.fetch(userId);
+
+            // If the user exists, push their info into the userList
             if (user) {
-                userList.push(user);
+                userList.push(`${userMention(userId)} - Role: ${role}, Kick in: ${duration} days`);
             }
         }
     }
+
     return userList;
 }
 
